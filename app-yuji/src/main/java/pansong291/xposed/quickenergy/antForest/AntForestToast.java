31e34d2871c33f364859f2aeb34d5eaf491a6bb9
case <442415631@qq.com>
2020-07-13 17:57:26 +0700

    增加注释

--- app-yuji/src/main/java/pansong291/xposed/quickenergy/antForest/AntForestToast.java
+++ app-yuji/src/main/java/pansong291/xposed/quickenergy/antForest/AntForestToast.java
@@ -1,40 +1,51 @@
 package pansong291.xposed.quickenergy.antForest;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.widget.Toast;
-
 import pansong291.xposed.quickenergy.hook.XposedHook;
 import pansong291.xposed.quickenergy.util.Config;
 import pansong291.xposed.quickenergy.util.Log;
 
+//蚂蚁森林吐司
 public class AntForestToast {
-    private static final String TAG = AntForestToast.class.getCanonicalName();
-    public static Context context;
+  //打印的tag
+  private static final String TAG = AntForestToast.class.getCanonicalName();
+  //Application
+  @SuppressLint("StaticFieldLeak")
+  public static Context context;
 
-    public static void show(CharSequence cs) {
-        try {
-            if (context != null && Config.showToast()) {
-                XposedHook.handler.post(
-                        new Runnable() {
-                            CharSequence cs;
-                            public Runnable setData(CharSequence c) {
-                                cs = c;
-                                return this;
-                            }
-                            @Override
-                            public void run() {
-                                try {
-                                    Toast.makeText(context, cs, Toast.LENGTH_SHORT).show();
-                                } catch (Throwable t) {
-                                    Log.i(TAG, "show.run err:");
-                                    Log.printStackTrace(TAG, t);
-                                }
-                            }
-                        }.setData(cs));
-            }
-        } catch (Throwable t) {
-            Log.i(TAG, "show err:");
-            Log.printStackTrace(TAG, t);
-        }
+  //消息吐司
+  public static void show(CharSequence cs) {
+    try {
+      if (context != null && Config.showToast()) {
+        XposedHook.handler.post(
+            //线程
+            new Runnable() {
+              //需要显示的文字
+              CharSequence cs;
+
+              //设置需要吐司的文字
+              public Runnable setData(CharSequence c) {
+                cs = c;
+                return this;
+              }
+
+              @Override
+              public void run() {
+                try {
+                  //执行吐司
+                  Toast.makeText(context, cs, Toast.LENGTH_SHORT).show();
+                } catch (Throwable t) {
+                  Log.i(TAG, "show.run err:");
+                  Log.printStackTrace(TAG, t);
+                }
+              }
+            }.setData(cs));
+      }
+    } catch (Throwable t) {
+      Log.i(TAG, "show err:");
+      Log.printStackTrace(TAG, t);
     }
+  }
 }
